a5f05405b9ad4c3ad39018c65db23622a1e5a38d
Carlos dos Santos <cds@cds-software.com.br>
2020-07-15 13:51:15 -0300

    cliente

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using apiazure.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace apiazure.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ILogger<ClientesController> _logger;
+
+        public ClientesController(ILogger<ClientesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public List<Clientes> Get()
+        {
+            var cli = new List<Clientes>();
+            cli.Add(new Clientes() {Id = 1, Nome = "Carlos" });
+            cli.Add(new Clientes() {Id = 2, Nome = "Joao" });
+            return cli;
+        }
+    }
+}
---
+++ Models/Clientes.cs
@@ -0,0 +1,9 @@
+namespace apiazure.Models
+{
+    public class Clientes
+    {
+        public int Id {get; set;}
+        public string Nome {get; set;}
+    }
+
+}
